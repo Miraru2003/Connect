512ee88ddc953c27b11a5ce978c491d4aafe472c
Miraru2003 <halloween_hayato2003@yahoo.co.jp>
2019-07-12 10:55:34 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,98 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require 'sinatra/activerecord'
+require './models'
+
+enable :sessions
+
+before '/tasks' do
+    if current_user.nil?
+        redirect '/'
+    end
+end
+
+helpers do
+    def current_user
+        User.find_by(id: session[:user])
+    end 
+end
+
+get '/' do
+    if current_user.nil?
+        @tasks = Task.none
+    else
+        @tasks = current_user.tasks
+    end
+    erb :index
+end
+
+get '/signup' do
+    erb :sign_up
+end
+
+post '/signup' do
+    user = User.create(
+        name: params[:name],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+        )
+    if user.persisted?
+        session[:user] = user.id
+    end
+    redirect '/'
+end
+
+get '/signin' do
+    erb :sign_in
+end
+
+post '/signin' do
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+    end
+    redirect '/'
+end
+
+get '/signout' do
+    session[:user] = nil
+    redirect '/'
+end
+
+get '/newtweet' do
+    erb :maketweet
+end
+
+post '/tasks' do
+    current_user.tasks.create(title: params[:tweet])
+    redirect '/'
+end
+
+post '/tasks/:id/delete' do
+    task = Task.find(params[:id])
+    task.destroy
+    redirect '/'
+end
+
+get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+end
+
+get '/tasks/new' do
+    erb :maketweet
+end
+
+get '/home' do 
+    erb :home
+end
+
+get '/news' do 
+    erb :news
+end
+
+get '/topmenu' do
+    erb :topmenu
+end
